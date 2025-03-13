7bc9b35b3674ec00482019a7ea5976f24071d425
codeveno <samsonochieng476@gmail.com>
2025-03-13 23:09:05 +0300

    deployment setup

--- main.py
+++ main.py
@@ -82,4 +82,7 @@ def camera_feed():
 
 # Run the Flask app
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 8080))  # Default to 8080 for smoother Render deployment
+    app.run(host="0.0.0.0", port=port)
+
